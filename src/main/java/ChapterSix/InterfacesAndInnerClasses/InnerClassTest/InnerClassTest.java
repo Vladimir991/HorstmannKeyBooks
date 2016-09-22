144ab9e4db0e5f8956a47b3da21fe652162aa48f
VladimirJava <spoolsv1@mail.ru>
2016-09-22 17:32:28 +0600

    we create example InnerClassTest. This program illustrates the use of inner classes.

---
+++ src/main/java/ChapterSix/InterfacesAndInnerClasses/InnerClassTest/InnerClassTest.java
@@ -0,0 +1,13 @@
+package ChapterSix.InterfacesAndInnerClasses.InnerClassTest;
+
+import javax.swing.*;
+
+public class InnerClassTest {
+    public static void main(String[] args) {
+        TalkingClock clock = new TalkingClock(1000, true);
+        clock.start();
+
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
+}
