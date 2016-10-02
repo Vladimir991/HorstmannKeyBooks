4e85970d0329b987c6da22588a0f8a97a5c44b40
VladimirJava <spoolsv1@mail.ru>
2016-10-02 18:21:46 +0600

    We create example DrawTest.

---
+++ src/main/java/ChapterSeven/GraphicsProgramming/DrawTest/DrawFrame.java
@@ -0,0 +1,10 @@
+package ChapterSeven.GraphicsProgramming.DrawTest;
+
+import javax.swing.*;
+
+public class DrawFrame extends JFrame {
+    public DrawFrame() {
+        add(new DrawComponent());
+        pack();
+    }
+}
