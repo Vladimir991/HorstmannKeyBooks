da956bf0a46bf2c07915647ebfcd8148bb6ae899
VladimirJava <spoolsv1@mail.ru>
2016-10-10 16:29:42 +0600

    We create example ButtonFrame.

---
+++ src/main/java/ChapterEight/EventHandling/ButtonFrame/ButtonMain.java
@@ -0,0 +1,12 @@
+package ChapterEight.EventHandling.ButtonFrame;
+
+import javax.swing.*;
+
+public class ButtonMain {
+    public static void main(String[] args) {
+        JFrame frame = new ButtonFrame();
+        frame.setTitle("ButtonFrame");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
