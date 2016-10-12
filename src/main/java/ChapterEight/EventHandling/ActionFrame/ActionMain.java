5872ed1c358b26c91d505df766691d80da4131f3
VladimirJava <spoolsv1@mail.ru>
2016-10-12 15:40:24 +0600

    we create example ActionFrame.

---
+++ src/main/java/ChapterEight/EventHandling/ActionFrame/ActionMain.java
@@ -0,0 +1,12 @@
+package ChapterEight.EventHandling.ActionFrame;
+
+import javax.swing.*;
+
+public class ActionMain {
+    public static void main(String[] args) {
+        JFrame frame = new ActionFrame();
+        frame.setTitle("ActionFrame");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
