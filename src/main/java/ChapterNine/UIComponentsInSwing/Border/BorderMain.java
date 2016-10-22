eca1bac82bcea80e8565572cd493fd42d530b515
VladimirJava <spoolsv1@mail.ru>
2016-10-22 15:19:47 +0600

    We create example  BorderFrame.

---
+++ src/main/java/ChapterNine/UIComponentsInSwing/Border/BorderMain.java
@@ -0,0 +1,11 @@
+package ChapterNine.UIComponentsInSwing.Border;
+
+import javax.swing.*;
+
+public class BorderMain {
+    public static void main(String[] args) {
+        JFrame frame = new BorderFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
