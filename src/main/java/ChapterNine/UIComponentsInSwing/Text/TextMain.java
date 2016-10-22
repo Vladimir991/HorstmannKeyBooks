66e9a5b9d4e2e033eef2c65715855be25ddd2d2a
VladimirJava <spoolsv1@mail.ru>
2016-10-22 12:59:58 +0600

    we created example TextComponentFrame.

---
+++ src/main/java/ChapterNine/UIComponentsInSwing/Text/TextMain.java
@@ -0,0 +1,13 @@
+package ChapterNine.UIComponentsInSwing.Text;
+
+import javax.swing.*;
+
+public class TextMain {
+    public static void main(String[] args) {
+        JFrame frame = new TextComponentFrame();
+        frame.setTitle("TextComponent");
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
