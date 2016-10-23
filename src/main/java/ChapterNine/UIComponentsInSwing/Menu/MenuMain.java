5850b3026b26b648dabd41acb128fba2dd7bd213
VladimirJava <spoolsv1@mail.ru>
2016-10-23 18:30:41 +0600

    We create example MenuFrame.

---
+++ src/main/java/ChapterNine/UIComponentsInSwing/Menu/MenuMain.java
@@ -0,0 +1,12 @@
+package ChapterNine.UIComponentsInSwing.Menu;
+
+import javax.swing.*;
+
+public class MenuMain {
+    public static void main(String[] args) {
+        JFrame frame = new MenuFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setTitle("MenuFrame");
+        frame.setVisible(true);
+    }
+}
