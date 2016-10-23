7e59da46a73b6fca4eff4d2ce6953acef460805a
VladimirJava <spoolsv1@mail.ru>
2016-10-23 18:45:46 +0600

    Add icons.

--- src/main/java/ChapterNine/UIComponentsInSwing/Menu/MenuFrame.java
+++ src/main/java/ChapterNine/UIComponentsInSwing/Menu/MenuFrame.java
@@ -61,11 +61,11 @@ public class MenuFrame extends JFrame {
         group.add(overtypeItem);
 
         Action cutAction = new TestAction("Cut");
-        cutAction.putValue(Action.SMALL_ICON, new ImageIcon("cut.gif"));
+        cutAction.putValue(Action.SMALL_ICON, new ImageIcon("resources/cut.gif"));
         Action copyAction = new TestAction("Copy");
-        copyAction.putValue(Action.SMALL_ICON, new ImageIcon("copy.gif"));
+        copyAction.putValue(Action.SMALL_ICON, new ImageIcon("resources/copy.gif"));
         Action pasteAction = new TestAction("Paste");
-        pasteAction.putValue(Action.SMALL_ICON, new ImageIcon("paste.gif"));
+        pasteAction.putValue(Action.SMALL_ICON, new ImageIcon("resources/paste.gif"));
 
         JMenu editMenu = new JMenu("Edit");
         editMenu.add(cutAction);
