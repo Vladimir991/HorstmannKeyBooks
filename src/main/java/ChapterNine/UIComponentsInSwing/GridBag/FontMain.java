056110c739e12b49ed5ce91479b43d4957b41318
VladimirJava <spoolsv1@mail.ru>
2016-10-24 18:07:48 +0600

    We create example FontFrame.

---
+++ src/main/java/ChapterNine/UIComponentsInSwing/GridBag/FontMain.java
@@ -0,0 +1,12 @@
+package ChapterNine.UIComponentsInSwing.GridBag;
+
+import javax.swing.*;
+
+public class FontMain {
+    public static void main(String[] args) {
+        JFrame frame = new FontFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setTitle("FontFrame");
+        frame.setVisible(true);
+    }
+}
