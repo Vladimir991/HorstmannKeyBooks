9261533de18b9f06b2e937e980fb397713ca2f75
VladimirJava <spoolsv1@mail.ru>
2016-10-25 17:36:00 +0600

    add files GroupLayout

---
+++ src/main/java/ChapterNine/UIComponentsInSwing/GroupLayout/FontMain.java
@@ -0,0 +1,12 @@
+package ChapterNine.UIComponentsInSwing.GroupLayout;
+
+import javax.swing.*;
+
+public class FontMain {
+    public static void main(String[] args) {
+        JFrame frame = new FontFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setTitle("FontFrame");
+        frame.setVisible(true);
+    }
+}
