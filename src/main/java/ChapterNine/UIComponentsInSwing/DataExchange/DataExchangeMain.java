a0ac90b9d9a2daf32910160b949963a9a2bc020c
VladimirJava <spoolsv1@mail.ru>
2016-10-26 14:44:46 +0600

    We created example DataExchangeFrame.

---
+++ src/main/java/ChapterNine/UIComponentsInSwing/DataExchange/DataExchangeMain.java
@@ -0,0 +1,11 @@
+package ChapterNine.UIComponentsInSwing.DataExchange;
+
+import javax.swing.*;
+
+public class DataExchangeMain {
+    public static void main(String[] args) {
+        JFrame frame = new DataExchangeFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
