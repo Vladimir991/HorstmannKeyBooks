1853ba6bff08b9ba3ab46a1d003622f7ba06a389
VladimirJava <spoolsv1@mail.ru>
2016-10-26 20:11:10 +0600

    Add file About.txt

--- src/main/java/ChapterTen/DeployingApplicationsAndApplets/Resource/ResourceTest.java
+++ src/main/java/ChapterTen/DeployingApplicationsAndApplets/Resource/ResourceTest.java
@@ -1,4 +1,15 @@
 package ChapterTen.DeployingApplicationsAndApplets.Resource;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class ResourceTest {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new ResourceTestFrame();
+            frame.setTitle("ResourceTest");
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
 }
--- src/main/java/ChapterTen/DeployingApplicationsAndApplets/Resource/ResourceTestFrame.java
+++ src/main/java/ChapterTen/DeployingApplicationsAndApplets/Resource/ResourceTestFrame.java
@@ -1,4 +1,26 @@
 package ChapterTen.DeployingApplicationsAndApplets.Resource;
 
-public class ResourceTestFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+
+public class ResourceTestFrame extends JFrame {
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 300;
+
+    public ResourceTestFrame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        URL aboutURL = getClass().getResource("resources/icon.gif");
+        Image image = new ImageIcon(aboutURL).getImage();
+        setIconImage(image);
+
+        JTextArea textArea = new JTextArea();
+        InputStream stream = getClass().getResourceAsStream("About.txt");
+        Scanner in = new Scanner(stream);
+        while (in.hasNext())
+            textArea.append(in.nextLine() + "\n");
+        add(textArea);
+    }
 }
