daba6de80982718f506b22b3cb7612488f76dfd1
VladimirJava <spoolsv1@mail.ru>
2016-10-30 20:08:30 +0600

    We created example ImageViewerFrame.

---
+++ src/main/java/ChapterEleven/ExceptionApprovalLoggingAndDebugging/Logging/ImageViewerFrame.java
@@ -0,0 +1,68 @@
+package ChapterEleven.ExceptionApprovalLoggingAndDebugging.Logging;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ImageViewerFrame extends JFrame {
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 400;
+
+    private JLabel label;
+    private static Logger logger = Logger.getLogger("kz.zhelezyaka");
+
+    public ImageViewerFrame() {
+        logger.entering("ImageViewerFrame", "<init>");
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu menu = new JMenu("File: ");
+        menuBar.add(menu);
+
+        JMenuItem openItem = new JMenuItem("Open");
+        menu.add(openItem);
+        openItem.addActionListener(new FileOpenListener());
+        JMenuItem exitItem = new JMenuItem("Exit");
+        menu.add(exitItem);
+        exitItem.addActionListener(e -> {
+            logger.fine("Exiting.");
+            System.exit(0);
+        });
+        label = new JLabel();
+        add(label);
+        logger.exiting("ImageViewerFrame", "<init>");
+    }
+
+    private class FileOpenListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            logger.entering("ImageViewerFrame.FileOpenListener", "actionPerformed", event);
+
+            JFileChooser chooser = new JFileChooser();
+            chooser.setCurrentDirectory(new File("."));
+
+            chooser.setFileFilter(new javax.swing.filechooser.FileFilter() {
+                public boolean accept(File f) {
+                    return f.getName().toLowerCase().endsWith(".gif") ||
+                            f.isDirectory();
+                }
+
+                public String getDescription() {
+                    return "GIF Images";
+                }
+            });
+            int r = chooser.showOpenDialog(ImageViewerFrame.this);
+
+            if (r == JFileChooser.APPROVE_OPTION) {
+                String name = chooser.getSelectedFile().getPath();
+                logger.log(Level.FINE, "Reading file {0}", name);
+                label.setIcon(new ImageIcon(name));
+            } else logger.fine("File open dialog canceled.");
+            logger.exiting("ImageViewerFrame.FileOpenListener", "actionPerformed");
+        }
+    }
+}
